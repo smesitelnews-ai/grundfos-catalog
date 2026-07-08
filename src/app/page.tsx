fa0825d04a448a5605dfe53a5452661ce6ef7f13
kashabadze-tech <kashabadze@gmail.com>
2026-07-08 03:08:11 +0300

    Bust Next.js image cache to show new pump images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,7 +15,7 @@ export default async function Home() {
     const hasRealImage = fs.existsSync(localPath);
     return {
       ...product,
-      image: hasRealImage ? imagePath : '/images/grundfos_pump.png'
+      image: hasRealImage ? imagePath + '?v=2' : '/images/grundfos_pump.png'
     };
   });
 
